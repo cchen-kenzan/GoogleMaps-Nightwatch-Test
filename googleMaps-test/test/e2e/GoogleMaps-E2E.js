909e0aaa6345ed47ec66cd590e46646df131566f
Claire Chen <cchen@kenzan.com>
2017-10-16 15:28:40 -0600

    Modified GoogleMaps-E2E.js to be more readable, cleaned up commented out coded, and added assertions and more steps to the E2E tests. Modified googleMaps.js to organize the page object elements so it's easier to use.

--- googleMaps-test/test/e2e/GoogleMaps-E2E.js
+++ googleMaps-test/test/e2e/GoogleMaps-E2E.js
@@ -1,84 +1,60 @@
 'use strict';
 
+const fromLocation = '1743 Wazee St, Denver, CO 80202';
+const toLocation = 'The Source, 3350 Brighton Blvd, Denver, CO 80216';
+
 module.exports = { 
   'Should Give Directions starting from fromLocation to toLocation': function(browser) {
-    var fromLocation = '1743 wazee street Denver, CO';
-    var toLocation = 'the source, denver, co';
-
     var googleMaps = browser.page.googleMaps();
     googleMaps.navigate()
       .clickforDirections()
-      .waitForElementVisible('@directionsFromTextBox')
-      .waitForElementVisible('@directionsToTextBox')
-      .setValue('@directionsFromTextBox', [fromLocation, browser.Keys.ENTER])
-      .setValue('@directionsToTextBox', [toLocation, browser.Keys.ENTER]);
-      //.assert.visible('@directionResults');
-      // googleMaps
-      //   .waitForElementVisible()
-    browser.saveScreenshot('google-directions-po.png');
-    // googleMaps
-    //   .setValue('@directionsFromTextBox', fromLocation)
-    //   .setValue('@directionsToTextBox', toLocation)
-    //   .submit();
-    //   browser.saveScreenshot('google-directions-submit-po.png');
-    //I need to come back to this, doesn't seem to find the @directionsWidget
-    // browser.waitForElementVisible('@directionsWidget', 5000, function() {
-    //   browser.saveScreenshot('google-directions-po.png');
-    //   var directionsWidget = googleMaps.section.directionsWidget;
-    //   console.log(directionsWidget);
-    // });
+      .waitForElementVisible('@directionsFromTextBox1')
+      .waitForElementVisible('@directionsToTextBox1')
+      .setValue('@directionsFromTextBox1', [fromLocation, browser.Keys.ENTER])
+      .setValue('@directionsToTextBox1', [toLocation, browser.Keys.ENTER])
+      .waitForElementVisible('@directionResults2')
+      .click('@firstDirectionDescription3')
+      .waitForElementVisible('@listedDirectionsFrom4');
 
-     //var directionsWidget = googleMaps.section.directionsWidget;
-    // //directionsWidget.expect.element('@directionsWidget').to.be.visible;
-    // console.log(directionsWidget);
-    // directionsWidget
-    //   .setValue('@directionsFromTextBox', fromLocation)
-    //   .setValue('@directionsToTextBox', toLocation)
-    //   .submit();
-    //   browser.saveScreenshot('google-directions-po.png');
-      //browser.end();
+    googleMaps.expect.element('@listedDirectionsFrom4').text.to.equal(fromLocation);
+    googleMaps.expect.element('@listedDirectionsTo4').text.to.equal(toLocation);
+
+    browser.saveScreenshot('google-directions-po.png');
+  },
+  'Should Reverse the directions': function(browser) {
+    var googleMaps = browser.page.googleMaps();
+
+    googleMaps
+      .click('@backToDiretionsList4')
+      .waitForElementVisible('@reverseDirectionsButton1')
+      .click('@reverseDirectionsButton1');
+
+    googleMaps.expect.element('@directionsFromTextBox1').value.to.equal(toLocation);
+    googleMaps.expect.element('@directionsToTextBox1').value.to.equal(fromLocation);
+
+    googleMaps
+      .click('@walkingDirectionsButton1')
+      .waitForElementVisible('@firstDirectionDescription3')
+      .click('@firstDirectionDescription3');
+
+    googleMaps.expect.element('@listedDirectionsFrom4').text.to.equal(toLocation);
+    googleMaps.expect.element('@listedDirectionsTo4').text.to.equal(fromLocation);
+
+    browser.saveScreenshot('google-reverse-directions-po.png');
 
   },
-  'Should Search Google Maps for Coors Fields and find the location': function(browser) {
+  'Should Search Google Maps for Coors Field and find the location': function(browser) {
     var searchCoors = 'coors field, denver, co';
     var googleMaps = browser.page.googleMaps();
     googleMaps.navigate()
       .setValue('@searchBox', [searchCoors, browser.Keys.ENTER]);
-      //.submitSearch();
+    googleMaps.expect.section('@searchResults').to.be.visible;
+
     var searchResults = googleMaps.section.searchResults;
-    //console.log(searchResults);
     searchResults.expect.element('@heroTitle').to.be.visible;
     browser.saveScreenshot('google-coorsFieldSearch-po.png');
-
-    var startingLocation = '1743 wazee street Denver, CO'
-    searchResults
-      .click('@directionsLink');
-  //     //.clickSearchResultDirections();
-    browser.saveScreenshot('google-coorsField-directions-po.png');
-    //used to debug:
-    // googleMaps.getValue('@directionsToTextBox', function(result) {
-    //   console.log("result: " + result);
-    //   console.log("type of " + typeof result);
-    //   console.log("value: " + result.value);
-    // });
-      googleMaps
-        .setValue('@directionsFromTextBox', [startingLocation, browser.Keys.ENTER]);
-      browser.saveScreenshot('google-coorsField-list-directions-po.png');
-    
-  //   var directionsWidget = googleMaps.section.directionsWidget;
-  //   directionsWidget
-  //     .setValue('@directionsFromTextBox', startingLocation)
-  //     .submit();
-  //     // .setValue()
 
     browser.end();
 
   }
-  // 'Should Display Directions to Coors Field': function(browser) {
-  //   var startingLocation = '1743 wazee street Denver, CO'
-  //   var googleMaps = browser.page.googleMaps();
-  //   googleMaps.navigate()
-  //   .
-  //   browser.end();
-  // }
 };
